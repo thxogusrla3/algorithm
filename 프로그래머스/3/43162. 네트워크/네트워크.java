6516f4a46e28412740a428f2c53a4e7f55a07f2d
thkim <64112259+thxogusrla3@users.noreply.github.com>
2025-09-14 21:44:23 +0900

    [level 3] Title: 네트워크, Time: 0.54 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int networkCount = 0;
+        boolean[] visited = new boolean[n];
+        
+        for(int i = 0 ; i < n; i++) {
+            if(!visited[i]) {
+                bfs(i, computers, visited);
+                networkCount++;
+            }
+        }
+        
+        
+        return networkCount;
+    }
+    
+    private void bfs(int start, int[][]computers, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        
+        while(!queue.isEmpty()) {
+            int current = queue.poll();
+            
+            for(int next = 0; next < computers.length; next++) {
+                if(next != current && computers[current][next] == 1 && !visited[next]) {
+                    visited[next] = true;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
+}
