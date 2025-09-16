e2b193b4c661a6ef0b53b5bf2e6934863722ef26
thkim <64112259+thxogusrla3@users.noreply.github.com>
2025-09-16 20:42:00 +0900

    [level 3] Title: 네트워크, Time: 0.48 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -7,36 +7,27 @@ class Solution {
         
         for(int i = 0; i < n; i++) {
             if(!visited[i]) {
-                dfs(i, computers, visited);
+                bfs(computers, visited, i);
                 answer++;
             }
         }
         return answer;
     }
     
-    //bfs는 큐를 활용
-    private void bfs(int start, int[][] computers, boolean[] visited) {
+    //
+    private void bfs(int[][] computers, boolean[] visited, int start) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(start);
+        visited[start] = true;
         
-        //현재보다 다음꺼를 찾기 위해
         while(!queue.isEmpty()) {
             int current = queue.poll();
             
-            for(int next = 0 ; next < computers.length; next++) {
-                if(next != current && computers[current][next] == 1 && !visited[next]) {
+            for(int next = 0; next < computers.length; next++) {
+                if(next != current && !visited[next] && computers[current][next] == 1) {
                     visited[next] = true;
                     queue.offer(next);
                 }
-            }
-        }
-    }
-    
-    private void dfs(int current, int[][] computers, boolean[] visited) {
-        visited[current] = true;
-        for(int next = 0; next < computers.length; next++) {
-            if(next != current && computers[current][next] == 1 && !visited[next]) {
-                dfs(next, computers, visited);
             }
         }
     }
