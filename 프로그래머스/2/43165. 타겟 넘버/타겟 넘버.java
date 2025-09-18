26fde048adb773e9a3cf939910bc175e4eb20f14
thkim <64112259+thxogusrla3@users.noreply.github.com>
2025-09-18 19:37:19 +0900

    [level 2] Title: 타겟 넘버, Time: 211.23 ms, Memory: 168 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,24 +3,39 @@ import java.util.*;
 class Solution {
     public int solution(int[] numbers, int target) {
         int answer = 0;
+        
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[] {0, 0});
         
         while(!queue.isEmpty()) {
-            int[] item = queue.poll();
-            int idx = item[0];
-            int sum = item[1];
+            int[] items = queue.poll();
+            int idx = items[0];
+            int sum = items[1];
             
             if(idx == numbers.length) {
-                if(sum == target) {
+                if(target == sum) {
                     answer++;
-                }   
+                }
             } else {
-                queue.offer(new int[]{idx + 1, sum + numbers[idx]});
-                queue.offer(new int[]{idx + 1, sum - numbers[idx]});
+                queue.offer(new int[] {idx + 1, sum + numbers[idx]});
+                queue.offer(new int[] {idx + 1, sum - numbers[idx]});
             }
         }
         
-        return answer;
+        System.out.println(answer);
+        
+        return answer;//dfs(numbers, target, 0, 0);
     }
+    
+    private int dfs(int[] numbers, int target, int idx, int sum) {
+        if(numbers.length == idx) {
+            return target == sum ? 1 : 0;
+        }
+        
+        int plus = dfs(numbers, target, idx + 1, sum + numbers[idx]);
+        int minus = dfs(numbers, target, idx + 1, sum - numbers[idx]);
+        
+        return plus + minus;
+    }
+    
 }
