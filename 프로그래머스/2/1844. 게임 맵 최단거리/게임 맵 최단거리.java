fc9a293323bfa7f49fc5bbefa434060df61976b1
thkim <64112259+thxogusrla3@users.noreply.github.com>
2025-09-20 15:38:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.21 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,44 +1,44 @@
 import java.util.*;
 
+//메모제이션 간 곳마다 카운트를 먹임.
 class Solution {
-    private final int[] dx = new int[] {1, -1, 0, 0};
-    private final int[] dy = new int[] {0, 0, 1, -1};
-    
     public int solution(int[][] maps) {
         int answer = 0;
-        
         int n = maps.length;
         int m = maps[0].length;
-        int[][] dist = new int[n][m];
         
-        if(maps[0][0] == 0 || maps[n-1][m-1] == 0) return -1;
+        if(maps[0][0] == 0 || maps[n - 1][m - 1] == 0) return -1;
+        
+        int[] dx = new int[]{1, -1, 0, 0};
+        int[] dy = new int[]{0, 0, 1, -1};
+        int[][] memo = new int[n][m];
+        memo[0][0] = 1;
         
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {0, 0});
+        queue.offer(new int[]{0, 0});
+        int nx, ny;
         
-        dist[0][0] = 1;
         while(!queue.isEmpty()) {
             int[] point = queue.poll();
             int x = point[0];
             int y = point[1];
             
             if(x == n - 1 && y == m - 1) {
-                return dist[x][y];
+                return memo[x][y];
             }
             
-            for(int i = 0 ; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            for(int i = 0; i < 4; i++) {
+                nx = x + dx[i];
+                ny = y + dy[i];
                 
-                if(nx < 0 || nx >= n || ny >= m || ny < 0) continue;
-                if(dist[nx][ny] != 0) continue;
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if(memo[nx][ny] != 0) continue;
                 if(maps[nx][ny] == 0) continue;
                 
-                dist[nx][ny] = dist[x][y] + 1;
-                queue.offer(new int[] {nx, ny});
+                memo[nx][ny] = memo[x][y] + 1;
+                queue.offer(new int[]{nx, ny});
             }
         }
-        
         return -1;
     }
 }
