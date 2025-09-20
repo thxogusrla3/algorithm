942111abcf8b33fc0730101cf5d1d2760c913bbe
thkim <64112259+thxogusrla3@users.noreply.github.com>
2025-09-20 14:39:51 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
 class Solution {
+    //시작하는 얘는 무조건 카운트 되어야함.
+    //방문했는지 체크 필요하고.
     public int solution(int n, int[][] computers) {
         int answer = 0;
         boolean[] visited = new boolean[n];
         
-        for(int i = 0 ; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             if(!visited[i]) {
-                visited[i] = true;
-                dfs(computers, visited, i);
+                bfs(computers, visited, i);
                 answer++;
             }
         }
-        
         return answer;
     }
     
@@ -24,20 +24,22 @@ class Solution {
         while(!queue.isEmpty()) {
             int current = queue.poll();
             
-            for(int next = 0; next < computers.length; next++) {
+            for(int next = 0 ; next < computers.length; next++) {
                 if(next != current && !visited[next] && computers[current][next] == 1) {
-                    queue.offer(next);
                     visited[next] = true;
+                    queue.offer(next);
                 }
             }
         }
     }
     
-    private void dfs(int[][] computers, boolean[] visited, int start) {
+    private void dfs(int[][] computers, boolean[] visited, int current) {
+        visited[current] = true;
+        
         for(int next = 0; next < computers.length; next++) {
-            if(next != start && !visited[next] && computers[start][next] == 1) {
-                visited[next] = true;
+            if(current != next && !visited[next] && computers[current][next] == 1) {
                 dfs(computers, visited, next);
+                visited[next] = true;
             }
         }
     }
