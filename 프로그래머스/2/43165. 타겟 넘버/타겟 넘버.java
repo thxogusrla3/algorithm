28c8bbfb87b35a33ccd5793989ae91d0cec1cff9
thkim <64112259+thxogusrla3@users.noreply.github.com>
2025-09-20 14:24:04 +0900

    [level 2] Title: 타겟 넘버, Time: 210.41 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,39 +1,37 @@
 import java.util.*;
 
 class Solution {
+    //1 또는 -1 을 번갈아가면서 해야하는데 5번 반복해야해
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        
+        int bfs = 0;
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {0, 0});
+        queue.offer(new int[]{0, 0});
         
         while(!queue.isEmpty()) {
-            int[] items = queue.poll();
-            int idx = items[0];
-            int sum = items[1];
+            int[] current = queue.poll();
+            int index = current[0];
+            int sum = current[1];
             
-            if(idx == numbers.length) {
+            if(index == numbers.length) {
                 if(target == sum) {
-                    answer++;
+                    bfs++;
                 }
             } else {
-                queue.offer(new int[] {idx + 1, sum + numbers[idx]});
-                queue.offer(new int[] {idx + 1, sum - numbers[idx]});
+                queue.offer(new int[] {index + 1, sum + numbers[index]});
+                queue.offer(new int[] {index + 1, sum - numbers[index]});
             }
         }
-        
-        System.out.println(answer);
-        
-        return answer;//dfs(numbers, target, 0, 0);
+        //return dfs(numbers, target, 0, 0);
+        return bfs;
     }
     
-    private int dfs(int[] numbers, int target, int idx, int sum) {
-        if(numbers.length == idx) {
+    private int dfs(int[] numbers, int target, int start, int sum) {
+        if(numbers.length == start) {
             return target == sum ? 1 : 0;
-        }
+        } 
         
-        int plus = dfs(numbers, target, idx + 1, sum + numbers[idx]);
-        int minus = dfs(numbers, target, idx + 1, sum - numbers[idx]);
+        int plus = dfs(numbers, target, start + 1, sum + numbers[start]);
+        int minus = dfs(numbers, target, start + 1, sum - numbers[start]);
         
         return plus + minus;
     }
