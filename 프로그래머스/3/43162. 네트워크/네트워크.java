97f79856fba0399021b62eb3ed61f073e7120380
thkim <64112259+thxogusrla3@users.noreply.github.com>
2025-10-03 16:50:59 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,32 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[computers.length];
-        
+        boolean[] visited = new boolean[n];
         for(int i = 0 ; i < n; i++) {
             if(!visited[i]) {
-                bfs(computers, visited, i);
                 answer++;
+                dfs(computers, visited, i);
             }
         }
+        
         return answer;
     }
     
-    private void bfs(int[][] computers, boolean[] visited, int start) {
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(start);
-        visited[start] = true;
+    private void dfs(int[][] computers, boolean[] visited, int current) {
+        visited[current] = true;
         
-        while(!q.isEmpty()) {
-            int current = q.poll();
-            
-            for(int next = 0; next < computers.length; next++) {
-                if(next != current && !visited[next] && computers[current][next] == 1) {
-                    visited[next] = true;
-                    q.add(next);
-                }
+        for(int next = 0; next < computers.length; next++) {            
+            if(current != next && !visited[next] && computers[current][next] == 1) {
+                dfs(computers, visited, next);
             }
         }
     }
