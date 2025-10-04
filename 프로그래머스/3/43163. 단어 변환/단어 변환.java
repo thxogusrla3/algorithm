afb966af86a2c389c1d5a0f7b6403d77ea20dac9
thkim <64112259+thxogusrla3@users.noreply.github.com>
2025-10-04 14:49:48 +0900

    [level 3] Title: 단어 변환, Time: 0.36 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,48 +2,40 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        if(!Arrays.asList(words).contains(target)) return 0;
-        
+        if(!Arrays.asList(words).contains(target)) {
+            return 0;
+        }         
         Queue<String> q = new ArrayDeque<>();
-        Map<String, Integer> map = new HashMap<>();
-        boolean[] visited = new boolean[words.length];
-        
+        Map<String, Integer> dist = new HashMap<>();
+        dist.put(begin, 0);
         q.add(begin);
-        map.put(begin, 0);
-        
-        while(!q.isEmpty()) {
-            String current = q.poll();
-            
-            if(current.equals(target)) {
-                return map.get(current);
-            }
-            
-            for(int i = 0 ; i < words.length; i++) {
-                if(visited[i]) continue;
-                
-                String next = words[i];
 
-                if(isDiffOne(next, current)) {
-                    map.put(next, map.get(current) + 1);
-                    q.add(next);
-                    visited[i] = true;
+        while(!q.isEmpty()) {
+            String start = q.poll();
+            if(start.equals(target)) return dist.get(start);
+
+            for(String nextWord: words) {                
+                if(isDiffOne(start, nextWord)) {
+                    if(dist.containsKey(nextWord)) continue;
+                    
+                    dist.put(nextWord, dist.get(start) + 1);
+                    q.add(nextWord);
                 }
             }
         }
-        
         return 0;
     }
     
-    private boolean isDiffOne(String compare, String target) {
+    private boolean isDiffOne(String start, String next) {
         int count = 0;
-        for(int i = 0 ; i < compare.length(); i++) {
-            char a = compare.charAt(i);
-            char b = target.charAt(i);
+        
+        for(int i = 0 ; i < start.length(); i++) {
+            char a = start.charAt(i);
+            char b = next.charAt(i);
             
-            if(a != b) {
-                count++;
-            }
+            if(a != b) count++;
         }
-        return count == 1;
+        
+        return count == 1 ? true : false;
     }
 }
