0d71220d7c074a991e316604d310724c57194ae1
thkim <64112259+thxogusrla3@users.noreply.github.com>
2025-10-09 14:56:06 +0900

    [level 3] Title: 아이템 줍기, Time: 1.03 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,40 +1,41 @@
 import java.util.*;
 
 class Solution {
-    private static final int[] dx = new int[] {1, -1, 0, 0};
-    private static final int[] dy = new int[] {0, 0, 1, -1};
-    private static final int MAX = 105;
+    private final int max = 105;
+    private static final int[] dx = {1, -1, 0, 0};
+    private static final int[] dy = {0, 0, 1, -1};
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
-        boolean[][] board = new boolean[MAX][MAX];
-        int[][] dist = new int[MAX][MAX];
+        boolean[][] board = new boolean[max][max];
+        int[][] dist = new int[max][max];
         
-        for(int[] r: rectangle) {
-            r[0] *= 2; r[1] *= 2; r[2] *= 2; r[3] *= 2;
-        }
         characterX *= 2; characterY *= 2;
         itemX *= 2; itemY *= 2;
         
-        for(int[] r: rectangle) {
-            for(int x = r[0]; x <= r[2]; x++) {
-                for(int y = r[1]; y <= r[3]; y++) {
+        for(int[] r : rectangle) {
+            r[0] *= 2; r[1] *= 2; r[2] *= 2; r[3] *= 2;
+        }
+        
+        for(int[] r : rectangle) {
+            for(int x = r[0]; x <=r[2]; x++) {
+                for(int y = r[1]; y <= r[3]; y ++) {
                     board[x][y] = true;
                     dist[x][y] = -1;
                 }
             }
         }
         
-        for(int[] r: rectangle) {
-            for(int x = r[0] + 1; x <= r[2] - 1; x++) {
-                for(int y = r[1] + 1; y <= r[3] - 1; y++) {
+        for(int[] r : rectangle) {
+            for(int x = r[0] + 1; x <=r[2] - 1; x++) {
+                for(int y = r[1] + 1; y <= r[3] - 1; y ++) {
                     board[x][y] = false;
                 }
             }
         }
         
         Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{characterX, characterY});
+        q.add(new int[] {characterX, characterY});
         dist[characterX][characterY] = 0;
         
         while(!q.isEmpty()) {
@@ -48,15 +49,16 @@ class Solution {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
                 
-                if(nx < 0 || ny < 0 || nx >= MAX || ny >= MAX) continue;
+                if(nx < 0 && ny < 0 && nx >= max && ny >= max) continue;
                 if(!board[nx][ny]) continue;
                 if(dist[nx][ny] != -1) continue;
                 
                 dist[nx][ny] = dist[cx][cy] + 1;
-                q.add(new int[]{nx, ny});
+                q.add(new int[] {nx, ny});
             }
         }
         
-        return 0;
+        
+        return -1;
     }
 }
