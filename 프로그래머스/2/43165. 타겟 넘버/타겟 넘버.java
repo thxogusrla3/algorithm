6507f5c8b5e90d3c12889fb79a94df5160df6f0b
thkim <64112259+thxogusrla3@users.noreply.github.com>
2025-10-22 08:39:52 +0900

    [level 2] Title: 타겟 넘버, Time: 121.06 ms, Memory: 167 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,25 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers, int target) {
-        int result = 0;
         Queue<int[]> q = new ArrayDeque<>();
+        int result = 0;
         q.add(new int[]{0, 0});
         while(!q.isEmpty()) {
-            int[] items = q.poll();
-            int idx = items[0];
-            int sum = items[1];
+            int[] qInfo = q.poll();
+            int idx = qInfo[0];
+            int sum = qInfo[1];
             
             if(idx == numbers.length) {
                 if(target == sum) {
                     result++;
                 }
-                
             } else {
-                q.add(new int [] {idx + 1, sum + numbers[idx]});
-                q.add(new int [] {idx + 1, sum - numbers[idx]});    
+                q.add(new int[]{ idx + 1, sum + numbers[idx]});
+                q.add(new int[]{ idx + 1, sum - numbers[idx]});
             }
         }
+        //return dfs(numbers, target, 0, 0);
         return result;
+        
     }
     
     private int dfs(int[] numbers, int target, int start, int sum) {
